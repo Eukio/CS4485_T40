85f97476702bf74cd5e04208c01102ed821c3ccb
chrisverde02 <chrisverderame02@gmail.com>
2026-05-02 21:26:59 -0500

    increased query size for some functions

--- src/main/java/com/example/test/db/DatabaseManager.java
+++ src/main/java/com/example/test/db/DatabaseManager.java
@@ -600,7 +600,7 @@ public String getAllWordsAlphaDESC() throws SQLException {
         FROM words
         WHERE word NOT IN ('[BEGIN]', '[END]')
         ORDER BY word DESC 
-        LIMIT 1000
+        LIMIT 2000
     """;
 
     StringBuilder sb = new StringBuilder();
@@ -644,7 +644,7 @@ public String getAllWordsAlphaDESC() throws SQLException {
         FROM words
         WHERE word NOT IN ('[BEGIN]', '[END]')
         ORDER BY word 
-        LIMIT 1000
+        LIMIT 2000
     """;
 
         StringBuilder sb = new StringBuilder();
@@ -689,7 +689,7 @@ public String getAllWordsAlphaDESC() throws SQLException {
         FROM words
         WHERE word NOT IN ('[BEGIN]', '[END]')
         ORDER BY total_count DESC 
-        LIMIT 1000
+        LIMIT 2000
     """;
 
         StringBuilder sb = new StringBuilder();
@@ -733,7 +733,7 @@ public String getAllWordsAlphaDESC() throws SQLException {
         FROM words
         WHERE word NOT IN ('[BEGIN]', '[END]')
         ORDER BY total_count 
-        LIMIT 1000
+        LIMIT 2000
     """;
 
         StringBuilder sb = new StringBuilder();
